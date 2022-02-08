7c3561ba64d172f59ff5587cca04ca6a4e4d0a32
Tess Monks <90337278+tessmonks@users.noreply.github.com>
2022-02-08 12:35:43 -0500

    Update wordle_solver.py

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,4 +1,3 @@
-from nltk.corpus import words
 import random
 from collections import defaultdict
 import re
@@ -6,3 +5,65 @@ import streamlit as st
 
 
 st.title('Wordle Solver')
+
+
+wordlist_name = 'common-5-words.txt'
+with open(wordlist_name) as fi:
+    
+    words_list = [word.strip() for word in fi]
+# filter for list of words that do not repeat letters t have max likelihood
+rgx = re.compile(r'.*(.).*\1.*') 
+def filter_words(word_list):
+    for word in word_list:
+        if rgx.match(word) is None:
+            yield word
+
+words_list = list(filter_words(words_list))
+
+word_string = ''.join(words_list)
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+counts = {}
+
+import random
+from collections import defaultdict
+import re
+
+wordlist_name = 'common-5-words.txt'
+with open(wordlist_name) as fi:
+    
+    words_list = [word.strip() for word in fi]
+# filter for list of words that do not repeat letters t have max likelihood
+rgx = re.compile(r'.*(.).*\1.*') 
+def filter_words(word_list):
+    for word in word_list:
+        if rgx.match(word) is None:
+            yield word
+
+words_list = list(filter_words(words_list))
+
+word_string = ''.join(words_list)
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+counts = {}
+
+# create a dictionary of probabilities for each letter 
+for letter in alpha:
+    counts[letter] = word_string.count(letter)
+probs = {}
+for letter in alpha:
+    probs[letter] = counts[letter]/len(word_string)
+
+# assign probability to each word given its letter
+word_probs = []
+for word in words_list:
+    probability = 1
+    for letter in word:
+        probability *= probs[letter]
+    word_probs.append((word, probability))
+
+# sort the list of all 5 letter words based on probabilities
+overall_probs = sorted(word_probs, key = lambda x: x[1], reverse = True)
+
+# randomly choose first word from 50 most probable
+first_word = random.choice(overall_probs[:50])[0]
+
+st.header("First word" + str(first_word))
