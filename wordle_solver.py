f8d38087a6537d28fed26bca68ad8965c84a2c01
Tess Monks <90337278+tessmonks@users.noreply.github.com>
2022-02-08 16:25:09 -0500

    Update wordle_solver.py

--- wordle_solver.py
+++ wordle_solver.py
@@ -1,7 +1,8 @@
 import random
 from nltk.corpus import words
+import sys
 import streamlit as st
-
+st.header("Wordle Solver")
 
 words_list = [word.lower() for word in words.words() if len(word) == 5]
 
@@ -28,46 +29,51 @@ for word in words_list:
 overall_probs = sorted(word_probs, key = lambda x: x[1], reverse = True)
 
 # randomly choose first word from 50 most probable
-first_word = random.choice(overall_probs[:50])[0]
+FIRST_WORD = random.choice(overall_probs[:50])[0]
 
 
-class Guess:
+class Rule:
     def __init__(self, letter, i=None):
-        word_bank = [word.upper() for word in words.words() if len(word)==5]
-        self.word_bank = word_bank
         self.letter, self.i = letter, i
-    
-class GreenLetter(Guess):
-    # keep words from the word bank with letters matching in the position guessed
-    code = 'G'
-    def apply(self, word_bank, matched_counts):
-        words = [word for word in word_bank if word[self.i] == self.letter]
+
+
+class RuleMatch(Rule):
+    code = '='
+    def apply(self, words, matched_counts):
+        words = [word for word in words if word[self.i] == self.letter]
         return words
-    
-class YellowLetter(Guess):
-    code = 'Y'
-    # keep words that contain the letter, but not in the position guessed
-    def apply(self, word_bank, matched_counts):
-        words = [word for word in word_bank if 
-                 self.letter in word and
-                word[self.i] != self.letter and 
-                matched_counts[self.letter] <= word.count(self.letter)]
+
+
+class RuleContainsElsewhere(Rule):
+    code = '+'
+    def apply(self, words, matched_counts):
+        # Only keep words which contain letter (not in position i, or else
+        # it would be an exact match (= not +) and which don't contain the
+        # letter more often than the number of counted matches.
+        words = [word for word in words if self.letter in word
+                    and word[self.i] != self.letter
+                    and matched_counts[self.letter] <= word.count(self.letter)]
         return words
-    
-class NoMatch(Guess):
-    code = 'B'
-    def apply(self, word_bank, matched_counts):
-        words = []
-        for word in word_bank:
-            # if the letter is not in the word at all
+
+
+class RuleExcludedLetter(Rule):
+    code = '-'
+    def apply(self, words, matched_counts):
+        _words = []
+        for word in words:
             if not matched_counts[self.letter] and self.letter in word:
+                # letter has not been matched anywhere in the word:
+                # don't include any words which have this letter.
                 continue
             if matched_counts[self.letter] > word.count(self.letter):
+                # letter has been matched n times: we can't include
+                # words that don't include it at least as many times.
                 continue
-            words.append(word)
+            _words.append(word)
+        words = _words[:]
         return words
-        
-Rule = {'G': GreenLetter, 'Y': YellowLetter, 'B': NoMatch}
+
+RuleCls = {'=': RuleMatch, '+': RuleContainsElsewhere, '-': RuleExcludedLetter}
 
 
 class Wordle:
@@ -77,77 +83,81 @@ class Wordle:
         if target_word:
             self.word_length = len(target_word)
 
-        self.words = [word.strip() for word in words.words() if len(word)==word_length]
-    
-    def test_word(self, guess):
-        
+        self.words = [word.lower() for word in words.words() if len(word) == 5]
+
+
+
+    def assess_word(self, test_word):
+
         target = list(self.target_word)
-        matched_letters = {}
-        rules = [None] * self.word_length
-        
-        # check the letters in guess against the target word
-        for i, letter in enumerate(guess):
+        matched_counts = {}
+        rules = [None] * 5
+        # Test test_word for the "exact match" and "excluded letter" rules.
+        for i, letter in enumerate(test_word):
             if letter == target[i]:
-                rules[i] = GreenLetter(letter, i)
+                rules[i] = RuleMatch(letter, i)
                 target[i] = '*'
                 matched_counts[letter] += 1
             elif letter not in target:
-                rules[i] = NoMatch(letter, i)
-                
-        for i, letter in enumerate(guess):
-            # check if letter is in word, just not in the current location
+                rules[i] = RuleExcludedLetter(letter, i)
+
+        for i, letter in enumerate(test_word):
             if rules[i]:
                 continue
-                
             if letter in target:
-                rules[i] = YellowLetter(letter, i)
+                # NB exact matches have already been filtered out.
+                rules[i] = RuleContainsElsewhere(letter, i)
                 target[target.index(letter)] = '*'
                 matched_counts[letter] += 1
             else:
-                rules[i] = NoMatch(letter, i)
+                rules[i] = RuleExcludedLetter(letter, i)
 
         rule_str = ''.join(rule.code for rule in rules)
         return rules, matched_counts, rule_str
-    
-    def rules(self, rule_codes, guess):
+
+
+    def parse_rule_codes(self, rule_codes, test_word):
         rules = []
-        matched_counts = {}
-        for i, letter in enumerate(guess):
-            rules.append(Rule[rule_codes[i]](letter, i))
-            if rule_codes[i] in 'GY':
+        matched_counts = defaultdict(int)
+        for i, letter in enumerate(test_word):
+            rules.append(RuleCls[rule_codes[i]](letter, i))
+            if rule_codes[i] in '+=':
                 matched_counts[letter] += 1
         return rules, matched_counts
 
-    
     def apply_rules(self, rules, matched_counts):
         for rule in rules:
             self.words = rule.apply(self.words, matched_counts)
-            
-    def choose_guess(self):
+
+
+    def get_test_word(self):
         k = random.choice(range(len(self.words)))
         return self.words[k], k
-    
-    def rules_input(self, guess):
-        guess = st.text_input("Input Colors")
-        return guess 
-    
-    def play(self):
+
+
+    def get_rules_input(self, test_word):
+        test_word = st.text_input("Letter Colors")
+        return test_word
+
+
+    def interactive(self):
         j = 0
-        init = first_word, self.words.index(first_word)
+        init = FIRST_WORD, self.words.index(FIRST_WORD)
         while len(self.words) > 1:
-            guess, k = self.choose_guess() if j else init
+            test_word, k = self.get_test_word() if j else init
             j += 1
-            rule_codes = self.rules_input(guess)
-            rules, matched_counts = self.rules(rule_codes,guess)
+            rule_codes = self.get_rules_input(test_word)
+            rules, matched_counts = self.parse_rule_codes(rule_codes,test_word)
             self.apply_rules(rules, matched_counts)
 
             if len(self.words) == 0:
-                sys.exit('no match')
+                sys.exit('I think you made a mistake: no words match this set'
+                         ' of rules.')
             elif len(self.words) == 1:
                 break
-            if guess in self.words:
-                del self.words[self.words.index(guess)]
-        st.header('The word is '+ str(Wordle.words[0]) +', found in ' + str(j) +' attempts.')
+            if test_word in self.words:
+                del self.words[self.words.index(test_word)]
+        st.header('The word is '+ str(self.words[0]) +', found in ' + str(j) +' attempts.')
 wordle = Wordle()
-wordle.play()
+wordle.interactive()
         
