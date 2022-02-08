7015115d6deaf890bb09b5cd1069b47b5c9d2753
Tess Monks <90337278+tessmonks@users.noreply.github.com>
2022-02-08 17:25:17 -0500

    Update wordle_solver.py

--- wordle_solver.py
+++ wordle_solver.py
@@ -140,6 +140,7 @@ class Wordle:
         j = 0
         init = first_word, self.words.index(first_word)
         if j == 0:
+            j += 1
             guess, k = init
             rule_codes = self.rules_input(guess)
             if rule_codes != None:
@@ -148,7 +149,7 @@ class Wordle:
         else:        
             while len(self.words) > 1:
 
-                guess, k = self.choose_guess() if j > 1 
+                guess, k = self.choose_guess()
                 j += 1
                 rule_codes = self.rules_input(guess)
                 rules, matched_counts = self.parse_rules(rule_codes,guess)
